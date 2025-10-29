888998ff6a4fd2682e865d57e4f8486cbcb34e36
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-10-29 15:03:33 +0100

    feat: aggiungo ciclo for su alcuni punti del bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -2,26 +2,38 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-/*  metodo array  */
+/*  CON METODO ARRAY */
 const reversedTeachers = teachers.reverse();
 console.log(reversedTeachers);
+/*  con ciclo */
+const reverse = [];
+for (let i = teachers.length - 1; i >= 0; i--) {
+  reverse.push(teachers[i]);
+}
+console.log(reverse);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+}
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-/* metodo array */
+/* CON METODO ARRAY */
 teachers.splice(5, 1);
 console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-/* metodo array */
+/* CON METODO ARRAY */
 const isFabioPresent = teachers.includes("Fabio");
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-/* Metodo array */
+/* CON METODO ARRAY */
 const teachersString = teachers.join(",");
 console.log(teachersString);
